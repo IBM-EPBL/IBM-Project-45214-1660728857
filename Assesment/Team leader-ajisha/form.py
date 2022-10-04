40c8168aaaa0d716f7a8298d50b3967f6f98e288
AjishaJegal <107546065+AjishaJegal@users.noreply.github.com>
2022-10-04 10:12:40 +0530

    Add files via upload

---
+++ Assesment/Team leader-ajisha/form.py
@@ -0,0 +1,19 @@
+from flask import Flask,render_template,request
+
+app = Flask(__name__,template_folder='templates')
+
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+database={'nach':'148','ajisha':'1418'} 
+
+
+@app.route('/form_login',methods=['POST','GET'])
+def gfg():
+   if request.method=="POST":
+       user_name= request.form.get("username")
+       return "Welcome "+user_name
+   return render_template("home.html")
+if __name__ == '__main__':
+    app.run(debug=True) 
